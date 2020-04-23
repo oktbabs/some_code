ac1d9e7cd86b99843a4cb561414f1a7fdef087dd
oracle <oracle@devopsmaster.mycompany.com>
2020-04-23 10:59:13 +0100

    Substituted + with * in script2 and another

--- testscript.sh
+++ testscript.sh
@@ -2,4 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE * $NUMBERTWO))
 echo "$NUMBER"
+echo "$NUMBER * $NUMBER "
 echo "This Timmy number one was added $NUMBER times a lot of divisions"
